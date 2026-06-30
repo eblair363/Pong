63434ef557dc573f7e73968bc6980dc3ce15744d
Ethan Flournoy <eflournoy03@gmail.com>
2026-06-30 17:42:35 -0400

    Moved on to the serializer in vivado. Preparing the merge into main and add serializer files.

--- rtl/tmds_encoder.sv
+++ rtl/tmds_encoder.sv
@@ -1,9 +1,10 @@
 module tmds_encoder (
-  input  logic        clk, rst,
-  input  logic [1:0]  ctrl,
-  input  logic [7:0]  d_in,
-  input  logic        blank,
-  output logic [9:0]  q_out
+    input  logic       clk,
+    rst,
+    input  logic [1:0] ctrl,
+    input  logic [7:0] d_in,
+    input  logic       blank,
+    output logic [9:0] q_out
 );
 
   //count ones, choose XOR vs XNOR
@@ -15,38 +16,36 @@ module tmds_encoder (
   always_comb begin
     q_m[0] = d_in[0];
     for (int i = 1; i <= 7; i++) begin
-      q_m[i] = use_xnor ? (q_m[i-1] ~^ d_in[i])   // XNOR
-                        : (q_m[i-1]  ^ d_in[i]);    // XOR
+      q_m[i] = use_xnor ? (q_m[i-1] ~^ d_in[i])  // XNOR
+      : (q_m[i-1] ^ d_in[i]);  // XOR
     end
     q_m[8] = ~use_xnor;  // 1 = used XOR, 0 = used XNOR
   end
 
-  //DC balance 
+  //DC balance
   wire [3:0] qm_ones = $countones(q_m[7:0]);
   wire signed [4:0] qm_balance = (5'(qm_ones) << 1) - 5'd8;
   logic signed [4:0] disparity, next_disparity;
   logic [9:0] q_encoded;
-  
+
   always_comb begin
-	if (disparity == 0 || qm_balance == 0) begin
-    //balanced, send as is
-		q_encoded = {1'b0, q_m[8], q_m[7:0]};
-		next_disparity = disparity + qm_balance;
-	end
-	else if ((disparity < 0 && qm_balance < 0) || (disparity > 0 && qm_balance > 0)) begin
-		//invert
-    q_encoded = {1'b1, q_m[8], ~q_m[7:0]};
-		next_disparity = disparity - qm_balance;
-	end
-	else begin
-		q_encoded = {1'b0, q_m[8], q_m[7:0]};
-		next_disparity = disparity + qm_balance;
-	end
+    if (disparity == 0 || qm_balance == 0) begin
+      //balanced, send as is
+      q_encoded = {1'b0, q_m[8], q_m[7:0]};
+      next_disparity = disparity + qm_balance;
+    end else if ((disparity < 0 && qm_balance < 0) || (disparity > 0 && qm_balance > 0)) begin
+      //invert
+      q_encoded = {1'b1, q_m[8], ~q_m[7:0]};
+      next_disparity = disparity - qm_balance;
+    end else begin
+      q_encoded = {1'b0, q_m[8], q_m[7:0]};
+      next_disparity = disparity + qm_balance;
+    end
   end
 
   always_ff @(posedge clk) begin
-	if (rst || blank) disparity <= 0;
-	else disparity <= next_disparity;
+    if (rst || blank) disparity <= 0;
+    else disparity <= next_disparity;
   end
 
 
@@ -66,3 +65,4 @@ module tmds_encoder (
   assign q_out = blank ? ctrl_token : q_encoded;
 
 endmodule
+
